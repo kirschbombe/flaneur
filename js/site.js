b1e638f9459967c4117c383a8827fcfa1fd7b5d3
dnoneill <dnoneill@ucla.edu>
2017-12-08 18:34:34 -0800

    getting ready for menu

--- js/site.js
+++ js/site.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
   console.log(map);
 });
+function reloadhtml(){
+  	url = window.location.href;
+  	if (url.includes("article")) {
+  		article_url = url.replace("/#", "");
+  		$.get(article_url, function(data){
+        	$("#sidebar-content").html(data);
+        });
+  	} else {
+  	about_url = url + "/about"
+  	$.get(about_url, function(data){
+  	console.log(data);
+  	$("#sidebar-content").html(data);
+  	 });
+  	}
+};
+
