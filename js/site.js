f33660888b7bc55f469f439ded8c8009868c0da2
dnoneill <dnoneill@ucla.edu>
2018-01-09 21:00:12 -0800

    closes #42, closes map popup, created article and page render functions

--- js/site.js
+++ js/site.js
@@ -4,49 +4,48 @@ $(document).ready(function(){
 function reloadhtml(){
   	url = window.location.href;
   	if (url.includes("article/")) {
-  		article_url = url.replace("/#", "");
-      url = url.split("#");
-      item_id = url[1];
-      marker = items[item_id];
-      var articleicon = "<img class='article-marker' src='" + marker.iconURL  + "' onclick='myFunction()'>";
-  		$.get(article_url, function(data){
-          var index = data.indexOf("</h1>");
-          data = data.slice(0, index) + articleicon + data.slice(index);
-          $("#sidebar-content").html(data);
-        });
-    marker.openPopup();
+  	  article_url = url.replace("/#", "");
+      item_id = url.split("#")[1];
+      articlerender(article_url, item_id);
   	} else if (url.includes("#") == true) {
-  	page_url = url.replace("/#", "");
-  	$.get(page_url, function(data){
-  	$("#sidebar-content").html(data);
-  	 });
+		page_url = url.replace("/#", "");
+		pagerender(page_url);
   	} else {
-    home_url = window.location.origin + window.location.pathname + "home/"
-    $.get(home_url, function(data){
-  	$("#sidebar-content").html(data);
-  	 });
+    	home_url = window.location.origin + window.location.pathname + "home/"
+    	pagerender(home_url);
     }
 };
+
+function articlerender(articleurl, item_id){
+    console.log(item_id);
+    marker = items[item_id];
+    var articleicon = "<img class='article-marker' src='" + marker.iconURL  + "' onclick='mapClick()'>";
+  	$.get(article_url, function(data){
+    	var index = data.indexOf("</h1>");
+        data = data.slice(0, index) + articleicon + data.slice(index);
+        $("#sidebar-content").html(data);
+    });
+   marker.openPopup();
+   $( ".sidebar" ).scrollTop(0); //tell sidebar scroll to go to the top
+}
+
+function pagerender(page_url){
+	$.get(page_url, function(data){
+		$("#sidebar-content").html(data);
+	  });
+	this.marker.closePopup();
+	$( ".sidebar" ).scrollTop(0); //tell sidebar scroll to go to the top
+}
 
 function onClick(url){
     if (url.includes("article/")) {
       item_id = url;
       article_url = window.location.origin + window.location.pathname + item_id;
-      marker = items[item_id];
-      var articleicon = "<img class='article-marker' src='" + marker.iconURL  + "' onclick='myFunction()'>";
-      $.get(article_url, function(data){
-          var index = data.indexOf("</h1>");
-          data = data.slice(0, index) + articleicon + data.slice(index);
-          $("#sidebar-content").html(data);
-        });
-    marker.openPopup();
+      articlerender(article_url, item_id);
     } else {
     	page_url = window.location.origin + window.location.pathname + url
-    	$.get(page_url, function(data){
-    		$("#sidebar-content").html(data);
-    	});
+    	pagerender(page_url);
     }
-    $( ".sidebar" ).scrollTop(0) //tell sidebar scroll to go to the top
 }
 
 
